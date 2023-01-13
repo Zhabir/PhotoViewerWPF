6b08cadd42e6d3f11e17dbc911bd5a6f88b49b3b
Zhabir <117615290+Zhabir@users.noreply.github.com>
2023-01-13 18:50:59 +0700

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static ВПФ_Фоторедактор.ImageList;
+using System;
+
+namespace ВПФ_Фоторедактор
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_exceptionWidthHeight()
+        {
+            var exception = Assert.ThrowsException<System.Exception>(() => ImageList.exceptionWidth(-1));
+        }
+        [TestMethod()]
+        public void DeleteCarriageTest()
+        {
+            var exception = Assert.ThrowsException<System.Exception>(() => ImageList.exceptionHeight(-1));
+
+        }
+    }
+}
